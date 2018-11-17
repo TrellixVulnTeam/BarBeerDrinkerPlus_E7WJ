9ed00184f2f68cfcc0857f63d0f27ab3ecc0e654
nikunjjhaveri123 <nikunjjhaveri@gmail.com>
2018-11-17 11:53:14 -0500

    db changes

--- bar-beer-drinker-ui/src/app/beer-details/beer-details.component.ts
+++ bar-beer-drinker-ui/src/app/beer-details/beer-details.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { BeerService, Beer } from '../beer.service';
 import { HttpResponse } from '@angular/common/http';
 
+declare const Highcharts: any;
+
 @Component({
   selector: 'app-beer-details',
   templateUrl: './beer-details.component.html',
@@ -23,6 +25,7 @@ export class BeerDetailsComponent implements OnInit {
         data => {
           this.beerDetails = data;
         },
+
         (error: HttpResponse<any>) => {
           if (error.status === 404) {
             alert('Beer not found');
@@ -32,11 +35,68 @@ export class BeerDetailsComponent implements OnInit {
           }
         }
       );
+      beerService.getBestSellingLocations(this.beerName).subscribe(
+        data => {
+          console.log(data);
+          var bars = [];
+          var sold = [];
+          data.sort((a, b) => a.BeersSold < b.BeersSold ? 1 : a.BeersSold > b.BeersSold ? -1 : 0)
+          data.forEach(bar => {
+            sold.push(bar.BeersSold);
+            bars.push(bar.Bar);
+          }
+        );
+        bars = bars.splice(0,10);
+        sold = sold.splice(0,10);
+        this.renderBeerChart(sold, bars);
+      });
     }
   );
   }
 
   ngOnInit() {
+  }
+
+  renderBestSellingBeersChart(drinkers: string[], spending: number[]) {
+    Highcharts.chart('barSpendersGraph', {
+      chart: {
+        type: 'column'
+      },
+      title: {
+        text: 'Largest Spenders'
+      },
+      xAxis: {
+        categories: drinkers,
+        title: {
+          text: 'Drinkers'
+        }
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: 'Amount drinker spends at this bar'
+        },
+        labels: {
+          overflow: 'justify'
+        }
+      },
+      plotOptions: {
+        bar: {
+          dataLabels: {
+            enabled: true
+          }
+        }
+      },
+      legend: {
+        enabled: false
+      },
+      credits: {
+        enabled: false
+      },
+      series: [{
+        data: spending
+      }]
+    });
   }
 
 }
