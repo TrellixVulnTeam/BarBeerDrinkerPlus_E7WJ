c6e3d2b88fee64b6252d7630e62efb075d354214
Kush Baldha <kush.baldha@gmail.com>
2018-11-17 12:11:46 -0500

    fixed drinker transactions

--- server/BarBeerDrinker/__init__.py
+++ server/BarBeerDrinker/__init__.py
@@ -49,6 +49,20 @@ def find_drinker(name):
     except Exception as e:
         return make_response(str(e), 500)
 
+@app.route('/api/drinker_transactions/<name>', methods=["GET"])
+def get_drinker_transactions(name):
+    try:
+        if name is None:
+            raise ValueError("Drinker is not specified.")
+        drinker = database.get_drinker_transactions(name)
+        if drinker is None:
+            return make_response("No drinker found with the given name.", 404)
+        return jsonify(drinker)
+    except ValueError as e:
+        return make_response(str(e), 400)
+    except Exception as e:
+        return make_response(str(e), 500)
+
 @app.route('/api/beers_cheaper_than', methods=["POST"])
 def find_beers_cheaper_than():
     body = json.loads(request.data)
